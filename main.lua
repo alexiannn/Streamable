40ecdda860b4c4e4b610a84f52ed6654eb089d11
alexian <113533408+alexiannn@users.noreply.github.com>
2022-11-08 16:37:43 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/idHN49At", true))()
+
+
+getgenv().Aiming.FOV = 16  -- use 10 or 14.5 fov for streamable
+getgenv().Aiming.FOVSides = 360
+getgenv().Aiming.HitChance = 100
+getgenv().Aiming.ShowFOV = false  -- change the "true" to "false" to not show the fov
+DahoodSettings.prediction = 0.131
+cframeSpheedhotkeyXd1 = "" -- its bad dont mind this
+
+getgenv().Aiming.UseMoreHitparts = true
+getgenv().Aiming.UseNearestCursor = true
+getgenv().Aiming.AutoPrediction = false
+getgenv().Aiming.KnockedCheck = true
+getgenv().Aiming.GrabbedCheck = true
+getgenv().Aiming.Wallcheck = true
