b8c9901f25f240958763297cc30c4cf463207395
alexian <113533408+alexiannn@users.noreply.github.com>
2022-11-10 20:14:41 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,3 +14,16 @@ getgenv().Aiming.AutoPrediction = false
 getgenv().Aiming.KnockedCheck = true
 getgenv().Aiming.GrabbedCheck = true
 getgenv().Aiming.Wallcheck = true
+
+local RunService = game:GetService("RunService")
+RunService.Heartbeat:Connect(function()
+    pcall(function()
+        for i,v in pairs(game.Players:GetChildren()) do
+            if v.Name ~= game.Players.LocalPlayer.Name then
+                local hrp = v.Character.HumanoidRootPart
+                hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)    
+                hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)   
+            end
+        end
+    end)
+end)
